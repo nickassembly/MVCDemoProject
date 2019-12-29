8dae5bf2af2651026717f652fbbc77e4afef156c
Nick Guerra <nickg@maliciousgames.com>
2019-12-28 19:47:59 -0600

    Added data access MVC

--- MVCApp/Controllers/HomeController.cs
+++ MVCApp/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MVCApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static  DataLibrary.BusinessLogic.EmployeeProcessor;
 
 namespace MVCApp.Controllers
 {
@@ -27,11 +29,48 @@ namespace MVCApp.Controllers
          return View();
       }
 
+      public ActionResult ViewEmployees()
+      {
+         ViewBag.Message = "Employees List";
+
+         var data = LoadEmployees();
+         List<EmployeeModel> employees = new List<EmployeeModel>();
+
+         foreach (var row in data)
+         {
+            employees.Add(new EmployeeModel
+            {
+               EmployeeId = row.EmployeeId,
+               FirstName = row.FirstName,
+               LastName = row.LastName,
+               EmailAddress = row.EmailAddress,
+               ConfirmEmail = row.EmailAddress
+            });
+         }
+
+
+         return View(employees);
+      }
+
       public ActionResult SignUp()
       {
          ViewBag.Message = "Employee Sign Up";
 
          return View();
       }
+
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public ActionResult SignUp(EmployeeModel model)
+      {
+         if (ModelState.IsValid)
+         {
+         int recoredsCreated = CreateEmployee(model.EmployeeId, model.FirstName, model.LastName, model.EmailAddress);
+            return RedirectToAction("Index");
+         }
+
+         return View();
+      }
+   
    }
 }
--- MVCApp/Models/EmployeeModel.cs
+++ MVCApp/Models/EmployeeModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,36 @@ namespace MVCApp.Models
 {
     public class EmployeeModel
     {
+      [Display(Name = "Employee ID")]
+      [Range(100000, 999999, ErrorMessage = "You need to enter a valid Employee Id")]
       public int EmployeeId { get; set; }
+      
+      [Display(Name = "First Name")]
+      [Required(ErrorMessage = "You need to give us your first name.")]
       public string  FirstName { get; set; }
+
+      [Display(Name = "Last Name")]
+      [Required(ErrorMessage = "You need to give us your last name.")]
       public string LastName { get; set; }
+
+      [DataType(DataType.EmailAddress)]
+      [Display(Name = "Email Address")]
+      [Required(ErrorMessage = "You need to give us an email address.")]
       public string EmailAddress { get; set; }
+
+      [Display(Name = "Confirm Email Address")]
+      [Compare("EmailAddress", ErrorMessage = "The Email and Confirm Email must match.")]
       public string ConfirmEmail { get; set; }
+
+      [Display(Name = "Password")]
+      [Required(ErrorMessage = "You must have a password.")]
+      [DataType(DataType.Password)]
+      [StringLength(100, MinimumLength = 10, ErrorMessage = "You need to provide a long enough password.")]
       public string Password { get; set; }
+
+      [Display(Name = "Confirm Password")]
+      [DataType(DataType.Password)]
+      [Compare("Password", ErrorMessage = "The Password and Confirm Password must match.")]
       public string ConfirmPassword { get; set; }
 
    }
